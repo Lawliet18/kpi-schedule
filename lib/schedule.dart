078cbad2b5f88bc0df6b843a26b2daa46387e307
Ivan Safin <dev148@devsteam.mobi>
2020-11-08 16:49:56 +0200

    wrong future

--- lib/schedule.dart
+++ lib/schedule.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:provider/provider.dart';
 import 'package:schedule_kpi/Models/lessons.dart';
 import 'package:schedule_kpi/http_response/parse_lessons.dart';
@@ -8,10 +9,22 @@ import 'package:schedule_kpi/settings.dart';
 
 import 'particles/lesson_block.dart';
 
-class Schedule extends StatelessWidget {
+class Schedule extends StatefulWidget {
   const Schedule({Key key, this.currentWeek}) : super(key: key);
 
   final String currentWeek;
+
+  @override
+  _ScheduleState createState() => _ScheduleState();
+}
+
+class _ScheduleState extends State<Schedule> {
+  SvgPicture imgOnErrorLoad;
+  @override
+  void initState() {
+    imgOnErrorLoad = SvgPicture.asset('assets/img/sad_smile.svg');
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -23,16 +36,25 @@ class Schedule extends StatelessWidget {
       'П’ятниця',
       'Субота'
     ];
-    String text = context.watch<Notifier>().groupName;
+    String text = Provider.of<Notifier>(context).groupName;
+    print(text);
     return text != null || text != ''
         ? DefaultTabController(
             length: list.length,
             child: Scaffold(
               backgroundColor: Colors.grey[200],
               appBar: AppBar(
-                leading: CurrentWeek(currentWeek: currentWeek),
-                title: Text('Розклад для ' + text.toUpperCase()),
-                centerTitle: true,
+                automaticallyImplyLeading: false,
+                title: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    CurrentWeek(currentWeek: widget.currentWeek),
+                    Spacer(),
+                    Text('Розклад для ' + text.toUpperCase()),
+                    Spacer()
+                  ],
+                ),
+                //centerTitle: true,
                 actions: [
                   IconButton(
                       icon: Icon(Icons.menu),
@@ -61,38 +83,98 @@ class Schedule extends StatelessWidget {
                   preferredSize: Size.fromHeight(30),
                 ),
               ),
-              body: Consumer<Notifier>(
-                builder: (context, value, child) => FutureBuilder(
-                    future: fetchLessons(text),
-                    builder: (BuildContext context, AsyncSnapshot snapshot) {
-                      List<Lessons> data = snapshot.data;
-                      return snapshot.hasData
-                          ? TabBarView(
-                              children: list
-                                  .map(
-                                    (e) => ListView.builder(
-                                      itemCount: data.length,
-                                      itemBuilder:
-                                          (BuildContext context, int index) {
-                                        if (e == data[index].dayName &&
-                                            data[index].lessonWeek ==
-                                                value.week) {
-                                          return LessonBlock(data: data[index]);
-                                        } else {
-                                          return Container();
-                                        }
-                                      },
-                                    ),
-                                  )
-                                  .toList())
-                          : Center(
-                              child: CircularProgressIndicator(),
-                            );
-                    }),
-              ),
-            ))
+              body: FutureBuilder(
+                  future: fetchLessons(text),
+                  builder: (BuildContext context, AsyncSnapshot snapshot) {
+                    List<Lessons> data = snapshot.data;
+                    //if(snapshot.connectionState == ConnectionState.)
+                    if (snapshot.connectionState == ConnectionState.none ||
+                        snapshot.hasError) {
+                      return buildOnWrongFuture(
+                          context,
+                          'Cannot load your schedule.\nPlease check your internet connection.',
+                          true);
+                    }
+                    if (snapshot.connectionState == ConnectionState.done &&
+                        data.isEmpty) {
+                      return buildOnWrongFuture(
+                          context, 'Input Your group correct', false);
+                    }
+                    return snapshot.connectionState == ConnectionState.done
+                        ? Consumer<Notifier>(
+                            builder: (context, value, child) => TabBarView(
+                                    children: list.map((e) {
+                                  return ListView.builder(
+                                    itemCount: data.length,
+                                    itemBuilder:
+                                        (BuildContext context, int index) {
+                                      if (e == data[index].dayName &&
+                                          data[index].lessonWeek ==
+                                              value.week) {
+                                        return LessonBlock(data: data[index]);
+                                      } else {
+                                        return Container();
+                                      }
+                                    },
+                                  );
+                                }).toList()))
+                        : Center(
+                            child: CircularProgressIndicator(),
+                          );
+                  }),
+            ),
+          )
         : Center(
             child: CircularProgressIndicator(),
           );
+  }
+
+  Center buildOnWrongFuture(
+      BuildContext context, String description, bool isInternetFailed) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        //crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Container(
+            alignment: Alignment.center,
+            width: 200,
+            height: 200,
+            padding: const EdgeInsets.all(0.0),
+            child: imgOnErrorLoad,
+          ),
+          SizedBox(height: 10),
+          Text(
+            description,
+            textAlign: TextAlign.center,
+            style: TextStyle(fontSize: 24, fontWeight: FontWeight.w500),
+          ),
+          SizedBox(height: 10),
+          isInternetFailed
+              ? FlatButton(
+                  onPressed: () {
+                    setState(() {});
+                  },
+                  child: Text(
+                    'Refresh',
+                    style:
+                        TextStyle(color: Theme.of(context).textSelectionColor),
+                  ),
+                  color: Theme.of(context).primaryColor,
+                )
+              : FlatButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: Text(
+                    'Change group',
+                    style:
+                        TextStyle(color: Theme.of(context).textSelectionColor),
+                  ),
+                  color: Theme.of(context).primaryColor,
+                )
+        ],
+      ),
+    );
   }
 }
