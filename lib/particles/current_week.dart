ee105b81deec1d632d0656d4b4eb097927ae4a6a
Ivan Safin <dev148@devsteam.mobi>
2020-11-08 16:49:31 +0200

    current week design

--- lib/particles/current_week.dart
+++ lib/particles/current_week.dart
@@ -21,7 +21,7 @@ class _CurrentWeekState extends State<CurrentWeek> {
   @override
   Widget build(BuildContext context) {
     TextStyle style = TextStyle(
-        color: Colors.black, fontSize: 16, fontWeight: FontWeight.bold);
+        color: Colors.black, fontSize: 18, fontWeight: FontWeight.bold);
     return GestureDetector(
       onTap: () {
         Provider.of<Notifier>(context, listen: false).changeWeek(current);
@@ -34,25 +34,45 @@ class _CurrentWeekState extends State<CurrentWeek> {
         });
       },
       child: Container(
-        margin: EdgeInsets.fromLTRB(15, 15, 10, 15),
-        padding: EdgeInsets.zero,
-        decoration: BoxDecoration(
-            color: Colors.white,
-            border: Border.all(color: Colors.transparent),
-            borderRadius: BorderRadius.circular(10)),
         child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          //mainAxisAlignment: MainAxisAlignment.spaceAround,
+          mainAxisAlignment: MainAxisAlignment.start,
           children: [
             Container(
-              color: current == '1' ? Colors.grey : Colors.white,
+              alignment: Alignment.center,
+              width: 20,
+              height: 25,
+              decoration: BoxDecoration(
+                color: current == '1' ? Colors.grey : Colors.white,
+                border: Border.all(color: Colors.transparent),
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(5),
+                  bottomLeft: Radius.circular(5),
+                ),
+              ),
               child: Text(
                 '1',
                 style: style,
+                textAlign: TextAlign.center,
               ),
             ),
             Container(
-                color: current == '2' ? Colors.grey : Colors.white,
-                child: Text('2', style: style))
+                width: 20,
+                height: 25,
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                  color: current == '2' ? Colors.grey : Colors.white,
+                  border: Border.all(color: Colors.transparent),
+                  borderRadius: BorderRadius.only(
+                    topRight: Radius.circular(5),
+                    bottomRight: Radius.circular(5),
+                  ),
+                ),
+                child: Text(
+                  '2',
+                  style: style,
+                  textAlign: TextAlign.center,
+                ))
           ],
         ),
       ),
