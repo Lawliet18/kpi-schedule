ed6d429dc4954bba06b43bbb6c56909210729609
Ivan Safin <dev148@devsteam.mobi>
2020-11-08 16:48:34 +0200

    try catch block add

--- lib/http_response/parse_groups.dart
+++ lib/http_response/parse_groups.dart
@@ -5,18 +5,21 @@ import 'package:schedule_kpi/Models/groups.dart';
 
 Future<List<Groups>> fetchGroups() async {
   List<Groups> list = [];
-  for (var i = 100; i <= 2400; i += 100) {
-    final response = await http.get(
-        'https://api.rozklad.org.ua/v2/groups/?filter={%27limit%27:100,%27offset%27:$i}');
-    if (response.statusCode == 200) {
-      String data = response.body;
-      final parsed = jsonDecode(data);
-      list.addAll(
-          parsed['data'].map<Groups>((json) => Groups.fromJson(json)).toList());
-    } else {
-      print('something wrong');
-      print(response.statusCode);
+  try {
+    for (var i = 100; i <= 2400; i += 100) {
+      final response = await http.get(
+          'https://api.rozklad.org.ua/v2/groups/?filter={%27limit%27:100,%27offset%27:$i}');
+      if (response.statusCode == 200) {
+        String data = response.body;
+        final parsed = jsonDecode(data);
+        list.addAll(parsed['data']
+            .map<Groups>((json) => Groups.fromJson(json))
+            .toList());
+      }
     }
+  } catch (e) {
+    print(e);
+    print('fetch group error');
   }
   return list;
 }
--- lib/http_response/parse_lessons.dart
+++ lib/http_response/parse_lessons.dart
@@ -4,16 +4,20 @@ import 'package:schedule_kpi/Models/lessons.dart';
 
 Future<List<Lessons>> fetchLessons(String text) async {
   List<Lessons> list = [];
-  final response =
-      await http.get('https://api.rozklad.org.ua/v2/groups/$text/lessons');
-  if (response.statusCode == 200) {
-    String data = response.body;
-    final parsed = jsonDecode(data);
-    list.addAll(
-        parsed['data'].map<Lessons>((json) => Lessons.fromJson(json)).toList());
-  } else {
-    print('something wrong');
-    print(response.statusCode);
+  try {
+    final response =
+        await http.get('https://api.rozklad.org.ua/v2/groups/$text/lessons');
+    if (response.statusCode == 200) {
+      String data = response.body;
+      final parsed = jsonDecode(data);
+      list.addAll(parsed['data']
+          .map<Lessons>((json) => Lessons.fromJson(json))
+          .toList());
+    }
+    return list;
+  } catch (e) {
+    print(e);
+    print('fecth lessons');
+    return [];
   }
-  return list;
 }
